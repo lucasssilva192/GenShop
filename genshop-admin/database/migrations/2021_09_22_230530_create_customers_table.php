83d26311ad7b99c4dd14b90839be341b0a246f2a
CelticSlash <henriquepereiralima@yahoo.com.br>
2021-09-22 20:56:21 -0300

    Criação tabela Customers + seeder

---
+++ genshop-admin/database/migrations/2021_09_22_230530_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birth_date');
+            $table->string('cpf');
+            $table->string('cellphone');
+            $table->string('telephone');
+            $table->string('profile_pic');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
