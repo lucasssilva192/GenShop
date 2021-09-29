3ccfe3189218e810b5f74473433a99efb8d13c35
Lucas Santos Silva <lucas.ssantos192@gmail.com>
2021-09-29 20:47:25 -0300

    User id na tabela customer

--- genshop-admin/database/migrations/2021_09_22_230530_create_customers_table.php
+++ genshop-admin/database/migrations/2021_09_22_230530_create_customers_table.php
@@ -16,6 +16,7 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('user_id');
             $table->string('first_name');
             $table->string('last_name');
             $table->date('birth_date');
