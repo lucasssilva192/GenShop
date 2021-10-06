730d50512752c191993919722ff792af108f950d
CelticSlash <henriquepereiralima@yahoo.com.br>
2021-10-06 20:33:42 -0300

    Código identado bonitão

--- genshop-admin/resources/views/order/index.blade.php
+++ genshop-admin/resources/views/order/index.blade.php
@@ -2,68 +2,68 @@
 
 @section('css')
 <style>
-      /* Carousel styling */
-      #introCarousel,
-      .carousel-inner,
-      .carousel-item,
-      .carousel-item.active {
-        height: 100vh;
-      }
+  /* Carousel styling */
+  #introCarousel,
+  .carousel-inner,
+  .carousel-item,
+  .carousel-item.active {
+    height: 100vh;
+  }
 
-      .carousel-item:nth-child(1) {
-        background-image: url('https://mdbootstrap.com/img/Photos/Others/images/76.jpg');
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: center center;
-      }
+  .carousel-item:nth-child(1) {
+    background-image: url('https://mdbootstrap.com/img/Photos/Others/images/76.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+  }
 
-      .carousel-item:nth-child(2) {
-        background-image: url('https://mdbootstrap.com/img/Photos/Others/images/77.jpg');
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: center center;
-      }
+  .carousel-item:nth-child(2) {
+    background-image: url('https://mdbootstrap.com/img/Photos/Others/images/77.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+  }
 
-      .carousel-item:nth-child(3) {
-        background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg');
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: center center;
-      }
+  .carousel-item:nth-child(3) {
+    background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+  }
 
-      /* Height for devices larger than 576px */
-      @media (min-width: 992px) {
-        #introCarousel {
-          margin-top: -58.59px;
-        }
-      }
+  /* Height for devices larger than 576px */
+  @media (min-width: 992px) {
+    #introCarousel {
+      margin-top: -58.59px;
+    }
+  }
 
-      .navbar .nav-link {
-        color: black !important;
-      }
+  .navbar .nav-link {
+    color: black !important;
+  }
 </style>
 @endsection
 
 @section('content')
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">GenShop Admin</a>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('store.index') }}" >Lojas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('product.index') }}" >Produtos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('order.index') }}" >Pedidos</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">GenShop Admin</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('store.index') }}">Lojas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('product.index') }}">Produtos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('order.index') }}">Pedidos</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </header>
 
 <body>
@@ -83,36 +83,33 @@
           <td>X</td>
           <td>Y</td>
           <td>Z</td>
-          <td> 
-                <a href="#" class="btn btn-primary btn-sm">Visualizar</a>
-                <a href="#" class="btn btn-primary btn-sm">Editar</a>
-        </td>
+          <td>
+            <a href="#" class="btn btn-primary btn-sm">Visualizar</a>
+            <a href="#" class="btn btn-primary btn-sm">Editar</a>
+          </td>
         </tr>
       </tbody>
-    </table>  
+    </table>
   </div>
-
 </body>
 
-  <!--Footer-->
-  <footer class="bg-light text-lg-start fixed-bottom">
-    
-    <div class="text-center py-4 align-items-center">
-      <a href="https://twitter.com/MDBootstrap" class="btn btn-primary m-1" role="button" rel="nofollow"
-        target="_blank">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="https://github.com/mdbootstrap/mdb-ui-kit" class="btn btn-primary m-1" role="button" rel="nofollow"
-        target="_blank">
-        <i class="fab fa-github"></i>
-      </a>
-    </div>
+<!--Footer-->
+<footer class="bg-light text-lg-start fixed-bottom">
 
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2020 Copyright:
-      <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-    </div>
-    <!-- Copyright -->
-  </footer>
+  <div class="text-center py-4 align-items-center">
+    <a href="https://twitter.com/MDBootstrap" class="btn btn-primary m-1" role="button" rel="nofollow" target="_blank">
+      <i class="fab fa-twitter"></i>
+    </a>
+    <a href="https://github.com/mdbootstrap/mdb-ui-kit" class="btn btn-primary m-1" role="button" rel="nofollow" target="_blank">
+      <i class="fab fa-github"></i>
+    </a>
+  </div>
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 @endsection
