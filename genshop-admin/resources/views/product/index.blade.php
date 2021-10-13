76c68534427fdc56d380011dceaae29a5289fd5e
CelticSlash <henriquepereiralima@yahoo.com.br>
2021-10-12 21:48:43 -0300

    Ajustes de front-end

--- genshop-admin/resources/views/product/index.blade.php
+++ genshop-admin/resources/views/product/index.blade.php
@@ -49,79 +49,38 @@
 @endsection
 
 @section('content')
-<header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">GenShop Admin</a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('store.index') }}">Lojas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('product.index') }}">Produtos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('order.index') }}">Pedidos</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-</header>
-
-<body>
-<h1> Produtos </h1>
-<a class="nav-link" href="{{ route('product.create') }}" >Novo</a>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Descrição</th>
-      <th scope="col">Preço</th>
-      <th scope="col">Opções</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($products as $product)
-    <tr>
-      <td>{{$product->id}}</td>
-      <td>{{$product->name}}</td>
-      <td>{{$product->description}}</td>
-      <td>{{$product->price}}</td>
-      <td> 
-            <a href="{{route('product.show', $product->id)}}" class="btn btn-primary btn-sm">Visualizar</a>
-            <a href="{{route('product.edit', $product->id)}}" class="btn btn-primary btn-sm">Editar</a>
-            <form class="d-inline" method="POST" action="{{ route('product.destroy', $product->id) }}" onsubmit="return remover();">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-primary btn-sm">Apagar</button>
-            </form>
-          </td>
+  <div class="container py-5">
+    <h1> Produtos </h1>
+    <a class="nav-link" href="{{ route('product.create') }}" >Novo</a>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Descrição</th>
+          <th scope="col">Preço</th>
+          <th scope="col">Opções</th>
         </tr>
+      </thead>
+      <tbody>
+        @foreach($products as $product)
+          <tr>
+            <td>{{$product->id}}</td>
+            <td>{{$product->name}}</td>
+            <td>{{$product->description}}</td>
+            <td>{{$product->price}}</td>
+            <td> 
+              <a href="{{route('product.show', $product->id)}}" class="btn btn-primary btn-sm">Visualizar</a>
+              <a href="{{route('product.edit', $product->id)}}" class="btn btn-primary btn-sm">Editar</a>
+              <form class="d-inline" method="POST" action="{{ route('product.destroy', $product->id) }}" onsubmit="return remover();">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-primary btn-sm">Apagar</button>
+              </form>
+            </td>
+          </tr>
         @endforeach
       </tbody>
     </table>
   </div>
-</body>
-
-<!--Footer-->
-<footer class="bg-light text-lg-start fixed-bottom">
-  <div class="text-center py-4 align-items-center">
-    <a href="https://twitter.com/MDBootstrap" class="btn btn-primary m-1" role="button" rel="nofollow" target="_blank">
-      <i class="fab fa-twitter"></i>
-    </a>
-    <a href="https://github.com/mdbootstrap/mdb-ui-kit" class="btn btn-primary m-1" role="button" rel="nofollow" target="_blank">
-      <i class="fab fa-github"></i>
-    </a>
-  </div>
-
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
-</footer>
 @endsection
