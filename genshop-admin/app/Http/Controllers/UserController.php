8aa31ca96ecee8f547fbbd6843292076b302cba0
Nicolas Alberto <darkathnva@gmail.com>
2021-11-13 04:33:34 -0300

    Update UserController.php

--- genshop-admin/app/Http/Controllers/UserController.php
+++ genshop-admin/app/Http/Controllers/UserController.php
@@ -18,7 +18,6 @@ class UserController extends Controller
         ]);
 
         $user = User::where('email', $request->email)->first();
-        $data = [$user->name, $user->email, $user->createToken($request->device_name)->plainTextToken];
         //Caso necessario validar durações do tokens para gerar novos.
         if(!$user || !Hash::check($request->password, $user->password)){
             error_log($request->email);
