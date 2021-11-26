7bd29285ce02211a12a8370fe4ee0fa10cc2dc4b
CelticSlash <henriquepereiralima@yahoo.com.br>
2021-11-26 20:57:58 -0300

    Logotipo Genshop e remoção de imagens

--- genshop-admin/resources/views/template/principal.blade.php
+++ genshop-admin/resources/views/template/principal.blade.php
@@ -31,7 +31,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/home">GenShop Admin</a>
+                <a class="navbar-brand" href="/home">@include('template.logo')</a>
                 <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fas fa-bars"></i>
                 </button>
